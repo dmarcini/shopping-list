9f17bc582512623f3fea97c20ced735b8cfa9b9b
Damian Marciniak <damian.marciniak.1996@gmail.com>
2020-10-14 16:23:30 +0200

    Implement localStorage initialization

--- src/index.js
+++ src/index.js
@@ -5,9 +5,16 @@ import App from "./components/App/App";
 
 import * as serviceWorker from "./js/serviceWorker";
 
+import LocalStorageManager from "./js/localStorageManager";
+
 import "./css/index.css";
 import "./css/fontawesome/all.min.css";
 import "bootstrap/dist/css/bootstrap.min.css";
+
+if (localStorage.getItem("shoppingLists") === null) {
+  localStorage.setItem("shoppingLists",
+                       JSON.stringify(new LocalStorageManager()));
+}
 
 ReactDOM.render(
   <App/>,
